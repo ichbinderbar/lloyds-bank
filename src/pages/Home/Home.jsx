51960addcee03d5f532cd88ea1bf8fe6c3f4d2bb
Olivia Burgess <burgess.olivia64@gmail.com>
2024-09-20 11:56:22 +0100

    jsx done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import "./Home.scss";
 import Notification from "../../components/Notification/Notification";
+import UpcomingPayments from "../../components/UpcomingPayments/UpcomingPayments";
 
 export default function Home() {
   return (
     <div className="home">
+      <UpcomingPayments />
       <Notification />
     </div>
   );
