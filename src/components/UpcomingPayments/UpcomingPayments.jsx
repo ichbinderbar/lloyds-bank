48d334f8233c7578abd21f66a475e03dbafeeda6
Olivia Burgess <burgess.olivia64@gmail.com>
2024-09-20 11:22:55 +0100

    initial commit

---
+++ src/components/UpcomingPayments/UpcomingPayments.jsx
@@ -0,0 +1,9 @@
+import "./UpcomingPayments.scss";
+
+export default function UpcomingPayments() {
+  return (
+    <section className="payments">
+      <div className="payments__title"> My Upcoming Payments</div>
+    </section>
+  );
+}
