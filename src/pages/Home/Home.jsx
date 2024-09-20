b56e34e6dc0ce22e622496241bf46a11188bb051
ichbinderbar <idrovel@gmail.com>
2024-09-20 12:26:14 +0100

    home page updates

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import "./Home.scss";
 import Notification from "../../components/Notification/Notification";
+import MainAccountOverview from "../../components/MainAccountOverview/MainAccountOverview";
 
 export default function Home() {
   return (
     <div className="home">
-      <Notification />
+      <div className="home__main-container">
+        <MainAccountOverview />
+        <Notification />
+      </div>
     </div>
   );
 }
